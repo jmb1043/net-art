3b81840726037dd7e1c0e544be89a6a335d6b7ef
jmb1043 <jmb1043@scarletmail.rutgers.edu>
2026-04-13 22:11:21 -0400

    Add files via upload

--- transformation/js/script.js
+++ transformation/js/script.js
@@ -1,24 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     
     const body = document.body;
-    const title = document.getElementById('title');
-    const flower = document.getElementById('flower');
 
-    document.addEventListener('dblclick', function() {
-        
-        if (body.classList.contains('zen-mode')) {
-            body.classList.remove('zen-mode');
-            body.classList.add('glitch-mode');
-            
-            title.textContent = "FATAL_ERROR: SYSTEM_OVERLOAD";
-            flower.textContent = "010110";
-        } else {
-            body.classList.remove('glitch-mode');
-            body.classList.add('zen-mode');
-            
-            title.textContent = "Breathe In.";
-            subtext.textContent = "Double-click to find peace.";
-            flower.textContent = "●";
-        }
+    document.addEventListener('mousedown', function() {
+        body.classList.remove('legal-mode');
+        body.classList.add('honest-mode');
+    });
+
+    document.addEventListener('mouseup', function() {
+        body.classList.remove('honest-mode');
+        body.classList.add('legal-mode');
+    });
+
+    document.addEventListener('mouseleave', function() {
+        body.classList.remove('honest-mode');
+        body.classList.add('legal-mode');
     });
 });
