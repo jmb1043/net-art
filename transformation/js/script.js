60bf25358b9cc8e8f3ca899ee9f231dbfa4dcb6d
jmb1043 <jmb1043@scarletmail.rutgers.edu>
2026-04-27 21:12:56 -0400

    Update script.js

--- transformation/js/script.js
+++ transformation/js/script.js
@@ -1,16 +1,6 @@
 document.addEventListener('mousemove', function(e) {
-    const body = document.body;
-    body.classList.add('scanner-active');
+    const truthLayer = document.getElementById('truth-layer');
 
-    const truthElements = document.querySelectorAll('.truth');
-
-    truthElements.forEach(el => {
-        const rect = el.getBoundingClientRect();
-        
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-
-        el.style.setProperty('--x', x + 'px');
-        el.style.setProperty('--y', y + 'px');
-    });
+    truthLayer.style.setProperty('--x', e.clientX + 'px');
+    truthLayer.style.setProperty('--y', e.clientY + 'px');
 });
